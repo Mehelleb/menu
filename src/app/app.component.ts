f31e134ba146f49fc22b02c9da0d8a29d5618fbc
yaline <archange34@hotmail.fr>
2025-10-09 09:20:14 +0200

    générer un composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PlatComponent } from './plat/plat.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+   imports: [PlatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'menu';
 }
