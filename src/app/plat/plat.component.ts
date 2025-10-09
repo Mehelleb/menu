261c102d1191ba9da2f5ca98d6e2b8aac1446385
yaline <archange34@hotmail.fr>
2025-10-09 10:04:52 +0200

    lier les données

--- src/app/plat/plat.component.ts
+++ src/app/plat/plat.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-plat',
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './plat.component.html',
   styleUrl: './plat.component.scss'
 })
-export class PlatComponent {
+export class PlatComponent implements OnInit {
+  name!:string;
+  description!:string;
+  createdAt!:Date;
+  like!:number;
+  imageUrl!:string;
+ngOnInit(): void {
+  this.name="Tacos";
+  this.description="Une viande au choix, des frites et de la sauce frmagère.";
+  this.createdAt= new Date();
+  this.like=0;
+  this.imageUrl = "https://t4.ftcdn.net/jpg/06/97/54/79/360_F_697547912_nImpPVXGEFh7RXrNkpEQOy5ksAqkKW8H.jpg"
+}
+  
 
 }
