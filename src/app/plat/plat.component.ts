473955934cbddc0c5f459c32de0f19505e41b3c2
yaline <archange34@hotmail.fr>
2025-10-09 19:28:08 +0200

    implémenter un bouton des likes

--- src/app/plat/plat.component.ts
+++ src/app/plat/plat.component.ts
@@ -12,13 +12,35 @@ export class PlatComponent implements OnInit {
   createdAt!:Date;
   like!:number;
   imageUrl!:string;
+  userLiked!:boolean;
+  btnTxtMsgLike!:string;
 ngOnInit(): void {
   this.name="Tacos";
   this.description="Une viande au choix, des frites et de la sauce frmagère.";
   this.createdAt= new Date();
   this.like=0;
   this.imageUrl = "https://t4.ftcdn.net/jpg/06/97/54/79/360_F_697547912_nImpPVXGEFh7RXrNkpEQOy5ksAqkKW8H.jpg"
+  this.userLiked=false;
+  this.btnTxtMsgLike="Liker";
 }
-  
+onLiking(){
+  if(this.userLiked){
+    this.onLike();
+  }else{
+    this.Unlike();}
+}
 
+onLike():void{
+  this.like--;
+  this.btnTxtMsgLike="Unliker";
+  this.userLiked=false; 
 }
+
+Unlike():void{
+  this.like++;
+  this.btnTxtMsgLike="Liker"
+  this.userLiked=true;
+ }
+}
+
+
